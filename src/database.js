f1c7bfd20d497a954ea5b77aaebfa5f040404099
Harold <haroldmedina18@hotmail.com>
2024-05-15 12:09:03 -0500

    conexion database cambio

--- src/database.js
+++ src/database.js
@@ -1,4 +1,6 @@
 const mongoose=require('mongoose')
 
-mongoose.connect('mongodb://mongo:AWYmeRpzhTXYIsUtPGGxXBZlzADZZVzV@roundhouse.proxy.rlwy.net:50505/pedidos')
+//mongoose.connect('mongodb://localhost:27017/pedidos')
+mongoose.connect('mongodb://mongo:AWYmeRpzhTXYIsUtPGGxXBZlzADZZVzV@roundhouse.proxy.rlwy.net:50505')
+
 
