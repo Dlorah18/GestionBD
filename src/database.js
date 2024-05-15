76aaf754bc3ec6d9e107e23a81c13f1195a91d7f
Harold <haroldmedina18@hotmail.com>
2024-05-15 11:43:12 -0500

    comando conexion

--- src/database.js
+++ src/database.js
@@ -1,3 +1,4 @@
 const mongoose=require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/pedidos')
+mongoose.connect('mongodb://mongo:AWYmeRpzhTXYIsUtPGGxXBZlzADZZVzV@roundhouse.proxy.rlwy.net:50505/pedidos')
+
